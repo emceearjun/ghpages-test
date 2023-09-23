a86579e64779731c98d662e7eda726033802981e
Arjun MC <mcarjun90@gmail.com>
2023-09-22 22:17:36 -0400

    added new route

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='p-4'>
+        <h1 className='text-2xl'>About</h1>
+        <p className='text-sm'>This is a paragraph about me</p>
+    </div>
+  )
+}
+
+export default About
